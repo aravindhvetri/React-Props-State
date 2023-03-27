c5aa669ad6c4b99b41dd54defc8906ba405230f7
aravindhvetri <2002aravinthv@gmail.com>
2023-03-27 10:49:29 +0530

    Adding.

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+// import Parant from './TASK-1/parent';
+// import Main from './TASK-2/main';
+// import Parent1 from './TASK-2/parent1';
+// import Parent2 from './TASK-1/parent2';
+import Main1 from './TASK-3(ClassComponent-State)/main1';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    {/* <Parant/> */}
+     {/* <Main/> */}
+    {/* <Parent1/> */} 
+    {/* <Parent2/> */}
+    <Main1/>
+  </div>
+);
+
+
